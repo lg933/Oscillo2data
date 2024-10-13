58f99dd73228358487195b3963b500a06ace328d
lg933 <leo.gallacio@epfl.ch>
2024-10-13 13:45:25 +0200

    Initial Commit

---
+++ C/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C
+//
+//  Created by Léo Gallacio on 13/10/2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
